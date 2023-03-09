324eccdb8e76c02c97e11739e5d434c30f96a78c
#C0KE <106424718+90-C0KE@users.noreply.github.com>
2023-03-09 14:48:12 +1100

    Update VictimApp_Opener.bat

--- Victim_App/VictimApp_Opener.bat
+++ Victim_App/VictimApp_Opener.bat
@@ -2,5 +2,5 @@
 ' -> Run BackBatch Victim App Invisibly
 
 Set WshShell = CreateObject("WScript.Shell") 
-WshShell.Run chr(34) & "C:\VictimApp.bat" & Chr(34), 0
+WshShell.Run chr(34) & "VictimApp.bat" & Chr(34), 0
 Set WshShell = Nothing
